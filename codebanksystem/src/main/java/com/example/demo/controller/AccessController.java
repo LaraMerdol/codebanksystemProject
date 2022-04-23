4466bba44ba52473990f98494e01c0d9647f018f
AmmaarIftikhar <hakim.ammaar@gmail.com>
2022-04-23 22:47:17 +0300

    coder companu classes

--- codebanksystem/src/main/java/com/example/demo/controller/AccessController.java
+++ codebanksystem/src/main/java/com/example/demo/controller/AccessController.java
@@ -1,8 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AccessController {
-    
+    @GetMapping("/") 
+    @ResponseBody
+    public String home(){
+        return  "Welcome home";
+    }
+
+    @GetMapping("/home") 
+    @ResponseBody
+    public String con(){
+        return  "cond";
+    }
 }
---
+++ codebanksystem/src/main/java/com/example/demo/model/Company.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Company extends User {
+    String companyName;
+    int workerCount;
+    int hiringStatus;
+    int budget;
+    double avgRate;
+    public Company( String user_id, String password, String email, String companyName,
+                                     int workerCount, int hiringStatus, int budget, double avgRate) {
+        super(user_id, password, email);
+        this.companyName = companyName;
+        this.workerCount = workerCount;
+        this.hiringStatus = hiringStatus;
+        this.budget = budget;
+        this.avgRate = avgRate;
+    }
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+    public int getWorkerCount() {
+        return workerCount;
+    }
+    public void setWorkerCount(int workerCount) {
+        this.workerCount = workerCount;
+    }
+    public int getHiringStatus() {
+        return hiringStatus;
+    }
+    public void setHiringStatus(int hiringStatus) {
+        this.hiringStatus = hiringStatus;
+    }
+    public int getBudget() {
+        return budget;
+    }
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+    public double getAvgRate() {
+        return avgRate;
+    }
+    public void setAvgRate(double avgRate) {
+        this.avgRate = avgRate;
+    }
+    @Override
+    public String toString() {
+        return "Company [avgRate=" + avgRate + ", budget=" + budget + ", companyName=" + companyName + ", hiringStatus="
+                + hiringStatus + ", workerCount=" + workerCount + "]";
+    }
+}
