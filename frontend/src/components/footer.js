b121602e5e4658a9c4543247b7ece1302c9e0aa4
LaraMerdol <laramerdol@gmail.com>
2022-04-24 14:52:38 +0300

    add new pages

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+class Footer extends Component {
+  render() {
+    return (
+        <div>
+          <Navbar  fixed='bottom'  bg='light' variant='light'>
+          <Container>
+          <NavbarToggle aria-controls='responsive-navbar-nav' />
+          <Nav className='bottom' >                
+              <Nav.Link href="/mainpage" >Home </Nav.Link>
+              <Nav.Link href="/searchpage" >Search </Nav.Link>
+              <Nav.Link href="/createactivitypage" >Create New Activity </Nav.Link>
+              <Nav.Link href="/discussionpage" >Discussion </Nav.Link>
+              <Nav.Link href="/profilepage" >Profile </Nav.Link>
+          </Nav>
+          </Container>
+          </Navbar>
+        </div>
+    );
+  }
+}
+
+export default Footer;
--- frontend/src/components/registerpage.js
+++ frontend/src/components/registerpage.js
@@ -1,112 +1,119 @@
-import React, { Component,useState } from 'react';
-import Form from 'react-bootstrap/Form'
+import React, { Component, useState } from "react";
+import Form from "react-bootstrap/Form";
+import { Navbar, Nav, Container, NavbarBrand } from "react-bootstrap";
+import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 export default function Register() {
-    const [user, setUser] = useState(null);
-    function setUserDiv(){
-      switch(user){
-          case "first":
-            return ;
-          case "coder":
-              return <div>
-              <form className="register-form">
-                <div className="form-group">
-                  <label>User ID</label>
-                  <input
-                    type="id"
-                    className="form-control"
-                    placeholder="Enter User ID"
-                  />
-                </div>
-                <div className="form-group ">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter Your Email"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Full Name</label>
-                  <input
-                    type="firstname"
-                    className="form-control"
-                    placeholder="First Name"
-                  />
-                    <input
-                    type="middlename"
-                    className="form-control"
-                    placeholder="Middle Name"
-                  />
-                    <input
-                    type="lastname"
-                    className="form-control"
-                    placeholder="Last Name"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Phone</label>
-                  <input
-                    type="phone"
-                    className="form-control"
-                    placeholder="Enter Your Phone Number"
-                  />
-                </div>
-    
-                <button type="submit" className="btn btn-dark btn-lg btn-block">
-                  Register 
-                </button>
-              </form>
-            </div>;
-          case "company":
-               return <div>
-              <form className="register-form">
-                <div className="form-group">
-                  <label>User ID</label>
-                  <input
-                    type="id"
-                    className="form-control"
-                    placeholder="Enter User ID"
-                  />
-                </div>
-                <div className="form-group ">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Enter password"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter Your Email"
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Company Name</label>
-                  <input
-                    type="name"
-                    className="form-control"
-                    placeholder="Enter Your Company Name"
-                  />
-                </div>
-                <button type="submit" className="btn btn-dark btn-lg btn-block">
-                  Register 
-                </button>
-              </form>
-            </div>;
-          case "editor":
-            return <div >
+  const [user, setUser] = useState(null);
+  function setUserDiv() {
+    switch (user) {
+      case "first":
+        return;
+      case "coder":
+        return (
+          <div>
+            <form className="register-form">
+              <div className="form-group">
+                <label>User ID</label>
+                <input
+                  type="id"
+                  className="form-control"
+                  placeholder="Enter User ID"
+                />
+              </div>
+              <div className="form-group ">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="form-group">
+                <label>Full Name</label>
+                <input
+                  type="firstname"
+                  className="form-control"
+                  placeholder="First Name"
+                />
+                <input
+                  type="middlename"
+                  className="form-control"
+                  placeholder="Middle Name"
+                />
+                <input
+                  type="lastname"
+                  className="form-control"
+                  placeholder="Last Name"
+                />
+              </div>
+              <div className="form-group">
+                <label>Phone</label>
+                <input
+                  type="phone"
+                  className="form-control"
+                  placeholder="Enter Your Phone Number"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-dark btn-lg btn-block">
+                Register
+              </button>
+            </form>
+          </div>
+        );
+      case "company":
+        return (
+          <div>
+            <form className="register-form">
+              <div className="form-group">
+                <label>User ID</label>
+                <input
+                  type="id"
+                  className="form-control"
+                  placeholder="Enter User ID"
+                />
+              </div>
+              <div className="form-group ">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="form-group">
+                <label>Company Name</label>
+                <input
+                  type="name"
+                  className="form-control"
+                  placeholder="Enter Your Company Name"
+                />
+              </div>
+              <button type="submit" className="btn btn-dark btn-lg btn-block">
+                Register
+              </button>
+            </form>
+          </div>
+        );
+      case "editor":
+        return (
+          <div>
             <form className="register-form">
               <div className="form-group">
                 <label>User ID</label>
@@ -141,36 +148,48 @@ export default function Register() {
                 />
               </div>
               <button type="submit" className="btn btn-dark btn-lg btn-block">
-                Register 
+                Register
               </button>
             </form>
-          </div>;
-          default:
-              return null;
-      }
-  }  
-    return (
-<div className="register-page">
-<div class="form">
-            <div className="form-group">
-            <h3>Register Page </h3>
-            
-            <Form.Select size="lg"
-                    onClick={(event) => {
-                        setUser(event.target.value);
-                    }}>
-              <option value="first">Select user type</option>        
-              <option value="coder">Coder</option>
-              <option value="company">Company</option>
-              <option value="editor">Editor</option>
-             </Form.Select >
-            </div>
-                        {setUserDiv()}
-            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+  return (
+    <div className="register-page">
+      <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+        <NavbarBrand>CodeBank</NavbarBrand>
+        <Container>
+          <NavbarToggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="nav_right">
+            <Nav>
+              <Nav.Link href="/">Welcome Page  </Nav.Link>
+              <Nav.Link href="/login">Login </Nav.Link>
+              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div class="form">
+        <div className="form-group">
+          <h3>Register Page </h3>
 
+          <Form.Select
+            size="lg"
+            onClick={(event) => {
+              setUser(event.target.value);
+            }}
+          >
+            <option value="first">Select user type</option>
+            <option value="coder">Coder</option>
+            <option value="company">Company</option>
+            <option value="editor">Editor</option>
+          </Form.Select>
+        </div>
+        {setUserDiv()}
       </div>
-    );
-
+    </div>
+  );
 }
-
-
